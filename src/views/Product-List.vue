9a8e0cd66b6e40f4e0d46b90db892162e34cabce
saad <professeurgamingyt@gmail.com>
2022-02-25 18:49:59 +0100

    Commit

--- src/views/Product-List.vue
+++ src/views/Product-List.vue
@@ -1,39 +1,94 @@
 <template>
   <div>
+    <div class="d-flex justify-content-end m-3">
+      <button type="button" class="btn btn-outline-success">
+        Create Product
+      </button>
+    </div>
     <div class="m-3">
       <!--Starting table-->
-      <table border="1" cellpadding="10" cellspacing="1" width="100%">
-        <tr style="background-color: LightGray">
-            <th>logo</th>
-            <th>produit name</th>
-            <th>stocke</th>
-            <th>prix</th>
+      <table class="table table-bordered">
+        <tr>
+          <th>product</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Stocke</th>
+          <th>Price</th>
+
+          <th width="150px">Action</th>
         </tr>
         <tr>
-            <td>logo</td>
-            <td>produit name</td>
-            <td>stocke</td>
-            <td>prix</td>
+          <td>image+product_name</td>
+          <td>Description...</td>
+          <td>Category 1</td>
+          <td>stocke</td>
+          <td>100 MAD</td>
+          <td align="center">
+            <button class="btn btn-danger mx-1">
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+            <button class="btn btn-primary mx-1">
+              <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+          </td>
         </tr>
         <tr>
-            <td>logo</td>
-            <td>produit name</td>
-            <td>stocke</td>
-            <td>prix</td>
+          <td>product</td>
+          <td>Description...</td>
+          <td>Category 1</td>
+          <td>stocke</td>
+          <td>100 MAD</td>
+          <td align="center">
+            <button class="btn btn-danger mx-1">
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+            <button class="btn btn-primary mx-1">
+              <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+          </td>
         </tr>
         <tr>
-            <td>logo</td>
-            <td>produit name</td>
-            <td>stocke</td>
-            <td>prix</td>
+          <td>product</td>
+          <td>Description...</td>
+          <td>Category 1</td>
+          <td>stocke</td>
+          <td>100 MAD</td>
+          <td align="center">
+            <button class="btn btn-danger mx-1">
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+            <button class="btn btn-primary mx-1">
+              <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+          </td>
         </tr>
       </table>
       <!--ending table-->
+    </div>
+    <div class="m-3">
+      <div class="form-group d-md-flex">
+        <div class="col-md-4"><label for="">Product Name: </label></div>
+        <div class="col-12 col-md-8"><input type="text" class="form-control" placeholder="Product name...."></div>
+      </div>
+<div class="input-group mb-3">
+  <input type="file" class="form-control" id="inputGroupFile02">
+  <label class="input-group-text" for="inputGroupFile02">Upload</label>
+</div>
     </div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      currency: "MAD",
+    };
+  },
   name: "Product-List",
 };
 </script>
+<style scoped>
+.table > tr {
+  border: 0;
+}
+</style>
