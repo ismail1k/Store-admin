1572e3bb1e148ce0f7b87c143f15d304353fe720
saad <professeurgamingyt@gmail.com>
2022-02-26 18:08:42 +0100

    Commit

---
+++ src/views/Customers.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <table class="table table-bordered">
+        <tr>
+          <th></th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th width="150px">Action</th>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <li class="list-group-item mx-1 px-4  "  >
+        <input
+          class="form-check-input me-1"
+          type="checkbox"
+        />
+        First checkbox
+      </li>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Customers",
+};
+</script>
