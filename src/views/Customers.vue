db1c75c2886ff0e0f39bb9d53ac41ccad329596e
saad <professeurgamingyt@gmail.com>
2022-02-26 20:34:49 +0100

    Commet

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
-    <div>
-      <table class="table table-bordered">
-        <tr>
-          <th></th>
-          <th>Description</th>
-          <th>Category</th>
-          <th>Quantity</th>
-          <th>Price</th>
-          <th width="150px">Action</th>
-        </tr>
-      </table>
-    </div>
-    <div>
-      <li class="list-group-item mx-1 px-4  "  >
-        <input
-          class="form-check-input me-1"
-          type="checkbox"
-        />
-        First checkbox
-      </li>
-    </div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col" class="opacity-75"> <h4>First</h4> </th>
+      <th scope="col" class="opacity-75"><h4>Gmail</h4></th>
+      <th scope="col" class="opacity-75"><h4>Last</h4></th>
+      <th scope="col" class="opacity-75"><h4>Handle</h4></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><strong > <h5>Mark Gaberiya</h5> </strong></td>
+      <td class="fs-5">Mark Gaberiya@gmail.com</td>
+      <td><select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select></td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td><strong > <h5>Jacob</h5> </strong></td>
+      <td>Otto</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td><strong > <h5>Larry</h5> </strong></td>
+      <td>Otto</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
   </div>
 </template>
 <script>
