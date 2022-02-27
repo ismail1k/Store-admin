0493338722c0bc6ad88da9fb10efd7b38378739a
saad <professeurgamingyt@gmail.com>
2022-02-27 23:22:37 +0100

    Commit

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,47 @@
 <template>
     <div class="container my-2">
-        Settings
+        <div>
+            <div class="form-group d-md-flex my-3">
+                <div class="col-md-4"><label for="">Select: </label></div>
+                <div class="col-12 col-md-8">
+                    <select class="form-select " aria-label="Default select example">
+                        <option>Yes</option>
+                        <option>No</option>
+                    </select>
+                </div>
+            </div>
+    <div class="form-group d-md-flex my-3">
+        <div class="col-md-4"><label for="">Input: </label></div>
+        <div class="col-12 col-md-8">
+            <input type="text" class="form-control" value="Input" placeholder="fullname" />
+        </div>
+    </div>
+        </div>
+        <div class="form-group d-md-flex my-3">
+                <div class="col-md-4"><label for="">Check box: </label></div>
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                <label class="form-check-label" for="flexCheckChecked">
+                    Checked checkbox
+                </label>
+        </div>
+        </div>
+        <div class="form-group d-md-flex my-3">
+                <div class="col-md-4"><label for="">Check box: </label></div>
+                <div class="form-check">
+               <input class="form-check-input" type="checkbox" role="switch"/>
+        <label class="form-check-label"  for="flexSwitchCheckDefault" >Switch</label
+        >
+        </div>
+        </div>
+        <div>
+             <div class="fw-bolder mt-5">Switch:</div>
+      <div class="form-check form-switch mt-3">
+        <input class="form-check-input" type="checkbox" role="switch"/>
+        <label class="form-check-label"  for="flexSwitchCheckDefault" >Switch</label
+        >
+      </div>
+        </div>
     </div>
 </template>
 <script>
