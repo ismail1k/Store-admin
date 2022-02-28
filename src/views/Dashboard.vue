4c6d4f0c45f33519eeb2adafd6b461ebf48e8843
saad <professeurgamingyt@gmail.com>
2022-02-28 20:08:40 +0100

    Commit

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,80 @@
 <template>
-    <div>Dashboard</div>
+    <div class="d-flex">
+        <div class="col-6 col-md-3 card border-light mb-3 shadow p-3 mb-5 bg-body rounded mx-2">
+            <div class=" font-monospace ml-3"> <b>Costumers</b> </div>
+            <div class="card-body">
+                <h5 class="card-title fw-normal fs-3 mt- ml-1">47k</h5>
+                <span class="badge rounded-pill bg-primary mt-3">+5%</span>
+            </div>
+        </div>
+        <div class="col-6 col-md-3 card border-light mb-3 shadow p-3 mb-5 bg-body rounded mx-2">
+            <div class=" font-monospace ml-3"> <b>Costumers</b> </div>
+            <div class="card-body">
+                <h5 class="card-title fw-normal fs-3 mt- ml-1">47k</h5>
+                <span class="badge rounded-pill bg-primary mt-3">+5%</span>
+            </div>
+        </div>
+        <div class="col-6 col-md-3 card border-light mb-3 shadow p-3 mb-5 bg-body rounded mx-2">
+            <div class=" font-monospace ml-3"> <b>Costumers</b> </div>
+            <div class="card-body">
+                <h5 class="card-title fw-normal fs-3 mt- ml-1">47k</h5>
+                <span class="badge rounded-pill bg-primary mt-3">+5%</span>
+            </div>
+        </div>
+        <div class="col-6 col-md-3 card border-light mb-3 shadow p-3 mb-5 bg-body rounded mx-2">
+            <div class=" font-monospace ml-3"> <b>Costumers</b> </div>
+            <div class="card-body">
+                <h5 class="card-title fw-normal fs-3 mt- ml-1">47k</h5>
+                <span class="badge rounded-pill bg-primary mt-3">+5%</span>
+            </div>
+        </div>
+    </div>
+    <div>
+      <div class="row">
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title opacity-75">Orders</h5>
+        <p class="card-text fs-3">15,450$</p>
+         <p>13,675</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3 mt-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title opacity-75">Items sold</h5>
+        <p class="card-text fs-3">15,450$</p>
+         <p>13,675</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title opacity-75">Refunds</h5>
+        <p class="card-text fs-3">15,450$</p>
+         <p>13,675</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title opacity-75 mt-3">Gross sale</h5>
+        <p class="card-text fs-3">15,450$</p>
+        <p>13,675</p>
+      </div>
+    </div>
+  </div>
+</div>  
+    </div>
+    
 </template>
 <script>
 export default {
-    name: 'Dashboard',
-}
+  name: "Dashboard",
+};
 </script>
