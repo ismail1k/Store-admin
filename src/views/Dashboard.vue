6436a9b5760ff490885a71a83fb5e34862d5659c
saad <professeurgamingyt@gmail.com>
2022-03-02 20:54:58 +0100

    Commit

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -78,17 +78,113 @@
                 </div>
             </div>
         </div>
-        <div>
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+    <div class="col-xxl-3 col-md-6 col-lg-5">
+              <div class="card shopping-cart-bar-min-height h-100">
+                <div class="card-header d-flex flex-between-center">
+                  <h6 class="mb-0">Shopping Cart</h6>
                 </div>
+                <div class="card-body py-0 d-flex align-items-center h-100">
+                  <div class="flex-1">
+                    <div class="row g-0 align-items-center pb-3">
+                      <div class="col pe-4">
+                        <h6 class="fs--2 text-600">Marc Gaberial</h6>
+                        <div class="progress" style="height:5px">
+                          <div class="progress-bar rounded-3 bg-primary" role="progressbar" style="width: 50% "></div>
+                        </div>
+                      </div>
+                      <div class="col-auto text-end">
+                        <p class="mb-0 fs--2 text-500 fw-semi-bold"> <span class="text-600">6817</span> </p>
+                      </div>
+                    </div>
+                     <div class="row g-0 align-items-center pb-3">
+                      <div class="col pe-4">
+                        <h6 class="fs--2 text-600">Marc Gaberial</h6>
+                        <div class="progress" style="height:5px">
+                          <div class="progress-bar rounded-3 bg-primary" role="progressbar" style="width: 50% "></div>
+                        </div>
+                      </div>
+                      <div class="col-auto text-end">
+                        <p class="mb-0 fs--2 text-500 fw-semi-bold"> <span class="text-600">6817</span> </p>
+                      </div>
+                    </div>
+                     <div class="row g-0 align-items-center pb-3">
+                      <div class="col pe-4">
+                        <h6 class="fs--2 text-600">Marc Gaberial</h6>
+                        <div class="progress" style="height:5px">
+                          <div class="progress-bar rounded-3 bg-primary" role="progressbar" style="width: 50% "></div>
+                        </div>
+                      </div>
+                      <div class="col-auto text-end">
+                        <p class="mb-0 fs--2 text-500 fw-semi-bold">  <span class="text-600">6817</span> </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            </div>
-    </div> 
+            <div class="col-xxl-9 col-md-12 mt-4">
+              <div class="card z-index-1">
+                <div class="card-header">
+                  <div class="row flex-between-center">
+                    <div class="col-6 col-sm-auto d-flex align-items-center pe-0">
+                      <h5 class="fs-0 mb-0 text-nowrap py-2 py-xl-0">Recent Costumers </h5>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body px-0 py-0">
+                  <div class="table-responsive scrollbar">
+                    <table class="table table-sm fs--1 mb-0 overflow-hidden">
+                      <thead class="bg-200 text-900">
+                        <tr>
+                          <th class="white-space-nowrap">
+                          </th>
+                          <th class="sort pe-1 align-middle white-space-nowrap">Customer</th>
+                          <th class="sort pe-1 align-middle white-space-nowrap">Email</th>
+                          <th class="sort pe-1 align-middle white-space-nowrap">Product</th>
+                          <th class="sort pe-1 align-middle white-space-nowrap text-center">Amount</th>
+                          <th class="sort pe-1 align-middle white-space-nowrap text-end">Roles</th>
+                          <th class="no-sort pe-1 align-middle data-table-row-action"></th>
+                        </tr>
+                      </thead>
+                      <tbody
+                       class="list"> <tr class="btn-reveal-trigger">
+                          <td class="align-middle" style="width: 28px;">
+                          </td>
+                          <th class="align-middle white-space-nowrap">Sylvia Plath</th>
+                          <td class="align-middle white-space-nowrap">john@gmail.com</td>
+                          <td class="align-middle white-space-nowrap">Slick - Drag &amp; Drop Bootstrap Generator</td>
+                          <td class="align-middle text-end">$99</td>
+                          <td class="align-middle text-end">Memeber</td>
 
+                          
+                        </tr>
+                        <tr class="btn-reveal-trigger">
+                          <td class="align-middle" style="width: 28px;">
+                          </td>
+                          <th class="align-middle white-space-nowrap ">Sylvia Plath</th>
+                          <td class="align-middle white-space-nowrap ">john@gmail.com</td>
+                          <td class="align-middle white-space-nowrap ">Slick - Drag &amp; Drop Bootstrap Generator</td>
+                          <td class="align-middle text-end">$99</td>
+                          <td class="align-middle text-end">Memeber</td>
+
+                        </tr>
+                        <tr class="btn-reveal-trigger">
+                          <td class="align-middle" style="width: 28px;">
+                          </td>
+                          <th class="align-middle white-space-nowrap ">Sylvia Plath</th>
+                          <td class="align-middle white-space-nowrap ">john@gmail.com</td>
+                          <td class="align-middle white-space-nowrap ">Slick - Drag &amp; Drop Bootstrap Generator</td>
+                          <td class="align-middle text-end">$99</td>
+                          <td class="align-middle text-end">memeber</td>
+
+                        </tr>
+                        </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
 </template>
 <script>
 export default {
