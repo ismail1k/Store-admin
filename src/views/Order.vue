dd9787009aa970105301ba748cb6f11a1b514f71
saad <professeurgamingyt@gmail.com>
2022-03-07 20:03:13 +0100

    Commit

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="container">Order Page</div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">First</th>
+      <th scope="col">More nfo</th>
+      <th scope="col">Status</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">product name</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 </template>
 <script>
 export default {
